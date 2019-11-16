1b562ea59a5536ab20d3cbd0784ab055b65f9501
poreniyi <57067964+poreniyi@users.noreply.github.com>
2019-11-16 11:14:08 -0500

    Update Reveresing an Array.js

--- Reveresing an Array.js
+++ Reveresing an Array.js
@@ -1,26 +1,21 @@
-function range(start,end,step=1){
-   let arr=[]
-  if (step<0){
-    for(let i=start;i>=end;i+=step){
-      arr.push(i)
-    }
-  }
-   
-  for(let i=start;i<=end;i++){
- 	arr.push(i);
-  }
+ function reverseArray(arr){
+   let arr2=[]
+   for(let i=arr.length-1;i>=0;i--){
+     arr2.push(arr[i]);
+   }
+   return arr2;
+ }
+function reverseArrayInPlace(arr){
+   let length=arr.length;
+   for(let i=0;i<Math.floor(arr.length/2);i++){
+     let temp=arr[i];
+     arr[i]=arr[length-i-1];
+     arr[length-i-1]=temp;
+   }
   return arr;
-}
-function sum(arr){
-  let result=0;
-  for(let number of arr){
-    result+=number;
-  }
-  return result
-}
-console.log(range(1, 10));
-// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(range(5, 2, -1));
-// → [5, 4, 3, 2]
-console.log(sum(range(1, 10)));
-// → 55
+ }
+//console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
